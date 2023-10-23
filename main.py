db03b8ee27f38b4de919ef22c56f122dd832ee76
Gurudeep Machupalli <gurumachupalli@gmail.com>
2023-10-23 19:29:25 -0400

    adding client machine creation

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import time
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# Generates a list of n number of client machines.
+def generate_client_machine_list(n):
+    client_machines = []
+    for client_machine in range(1, n+1):
+        client_machines.append(client_machine)
+    return client_machines
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+if __name__ == "__main__":
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # Gives the user the option to enter how many list sizes they want.
+    n_list = int(input("Enter n lists: "))
+
+    # Generates n number of client machines
+    list_of_client_machines = generate_client_machine_list(n=n_list)
+
+    print(list_of_client_machines)
+
+    # Start time (in ns) of when the function to implement runs.
+    start = time.perf_counter_ns()
+
+    # TODO FUNCTION TO IMPLEMENT
+
+    # End time (in ns) of when the function to implement runs.
+    end = time.perf_counter_ns()
