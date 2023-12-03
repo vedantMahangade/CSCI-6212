62afe3b21b724a3b7edc8c8057ab3865e9ebbafc
MiguelMazumder <72771218+MiguelMazumder@users.noreply.github.com>
2023-12-03 11:40:26 -0500

    Update P_4_Generalized_assignment_problem.py
    Time elapsed is not calculated

--- P_4_Generalized_assignment_problem.py
+++ P_4_Generalized_assignment_problem.py
@@ -1,9 +1,16 @@
 import numpy as np
 import sys
+import time
+
+#Initialize employee-job matrix
+time_elapsed = np.zeros((1, 100))
 
 def calculate_total_payment(assignment, costs):
+    # Initialize total payment
     total_payment = 0
+    # Iterate through people in the mxm matrix
     for person, job in enumerate(assignment):
+        # Add their payment when assigned to a certain job (assignment)
         total_payment += costs[person, job]
     return total_payment
 
@@ -11,8 +18,11 @@ def branch_and_bound(assignment, costs, current_payment, remaining_jobs, best_so
     if remaining_jobs == 0:
         # Leaf node, calculate actual payment
         current_payment += calculate_total_payment(assignment, costs)
+        # Check if current_payment is lower than best_solution
         if current_payment < best_solution[0]:
+            # If solution is more optimal that best solution, replace it
             best_solution[0] = current_payment
+            # Copy best_solution
             best_solution[1] = assignment.copy()
         return
 
@@ -32,10 +42,13 @@ def solve_generalized_assignment(costs):
     branch_and_bound([], costs, 0, n, best_solution)
     return best_solution[1]
 
-# Example usage:
-m = 3  # Number of people
-n = 3  # Number of jobs
-costs = np.random.randint(1, 100, size=(m, n))  # Random costs for each assignment
-
-best_assignment = solve_generalized_assignment(costs)
-print("Best assignment:", best_assignment)
+for i in range(1,100):
+    m = i  # Number of people and jobs
+    costs = np.random.randint(1, 100, size=(m, m))  # Random costs for each assignment
+    start = time.perf_counter_ns()
+    best_assignment = solve_generalized_assignment(costs)
+    #print(costs)
+    #print("Best assignment:", best_assignment)
+    end = time.perf_counter_ns()
+    time_elapsed[0, i] = end-start
+print(time_elapsed)
